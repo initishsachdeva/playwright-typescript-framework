e1177f2fac08b5358ae0a9e1f89be2d695e8262a
initishsachdeva <nitishsachdeva39@gmail.com>
2023-04-26 00:28:20 +0530

    basic framework files

---
+++ config.ts
@@ -0,0 +1,45 @@
+import {PlaywrightTestConfig} from '@playwright/test';
+
+const config: PlaywrightTestConfig = {
+    globalSetup: require.resolve('./global-setup'),
+    testDir: 'tests/',
+    testMatch: /.*\.js/,
+    grep: [
+        /Login/
+    ],
+    timeout: 7200000,
+    retries: 0,
+    snapshotDir: 'snapShots',
+    workers: 1,
+    reporter: [['allure-playwright'], ['html', {open: 'never'}], ['line']],
+    use: {
+        storageState: 'state.json',
+        baseURL: 'https://www.canva.com/',
+        actionTimeout: 30000,
+        ignoreHTTPSErrors: true,
+        video: 'off',
+        screenshot: 'only-on-failure',
+        channel: "chrome"
+    },
+    projects: [
+        {
+            name: 'pipeline',
+            use: {
+                headless: true,
+                viewport: {width: 1920, height: 1080}
+            },
+        },
+        {
+            name: 'local',
+            use: {
+                headless: false,
+                viewport: null,
+                launchOptions: {
+                    args: ["--start-maximized"]
+                },
+            },
+        }
+    ]
+}
+
+export default config;
